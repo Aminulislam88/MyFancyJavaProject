ce1035b9b12a0d148c7a798cb3f36df5efed5aab
Aminulislam88 <aislam181043@bscse.uiu.ac.bd>
2022-05-13 14:54:50 +0600

    initial commit

--- src/myfancyjavaproject/MyFancyJavaProject.java
+++ src/myfancyjavaproject/MyFancyJavaProject.java
@@ -1,29 +1,28 @@
-// C++ program to find GCD of two numbers
-#include <iostream>
-using namespace std;
-// Recursive function to return gcd of a and b
-int gcd(int a, int b)
-{
-	// Everything divides 0
-	if (a == 0)
-	return b;
-	if (b == 0)
-	return a;
 
-	// base case
-	if (a == b)
+class Test
+{
+	
+	static int gcd(int a, int b)
+	{
+		
+		if (a == 0)
+		return b;
+		if (b == 0)
 		return a;
-
-	// a is greater
-	if (a > b)
-		return gcd(a-b, b);
-	return gcd(a, b-a);
-}
-
-// Driver program to test above function
-int main()
-{
-	int a = 98, b = 56;
-	cout<<"GCD of "<<a<<" and "<<b<<" is "<<gcd(a, b);
-	return 0;
+	
+	
+		if (a == b)
+			return a;
+	
+		if (a > b)
+			return gcd(a-b, b);
+		return gcd(a, b-a);
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		int a = 98, b = 56;
+		System.out.println("GCD of " + a +" and " + b + " is " + gcd(a, b));
+	}
 }
